ac937e2b721fc22c385404430f6af8d351406ca0
rodriguezMujica <gabrielrodriguez.germ@gmail.com>
2024-02-12 16:35:35 -0400

    primeros seis ejercicios del test

---
+++ M05 Fundamentos Javascript/demo.js
@@ -0,0 +1,45 @@
+// var stringVacio = "";
+// var string = "palabra";
+// var stringVacio = "texto largo ";
+
+
+// var numero
+
+
+// console.log(string)
+
+
+// console.log(typeof string)
+
+
+function suma() {
+    var numero1 = 5
+    var numero2 = 6
+    var numero3 = 7
+    var numero4 = 8
+
+    console.log(numero1 * numero2 + numero4 * numero2)
+
+        return 0;
+}
+
+suma()
+
+
+
+function saludar(nombre) {
+    console.log("hola " + nombre)
+}
+
+saludar("mario")
+
+var edad = 29
+function validacion(edad){
+    if(edad > 18) {
+        console.log("es mayor de edad")
+    } else {
+        console.log("es menor de edad ")
+    }
+}
+
+validacion(edad)
